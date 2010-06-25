2e25e3992c7e23d577e381e45f52b922ecdc0bf2
Nick Peelman <nick@peelman.us>
2010-06-25 16:25:06 -0400

    adding Passenger config to httpd conf file and adding sample to ConfigFile class.

--- PassengerApplicationConfigFile.m
+++ PassengerApplicationConfigFile.m
@@ -10,4 +10,17 @@
 
 @implementation PassengerApplicationConfigFile
 
+/*  Sample Config File
+
+ <VirtualHost *:80>
+ ServerName www.yourhost.com
+ DocumentRoot /somewhere/public    # <-- be sure to point to 'public'!
+ <Directory /somewhere/public>
+ AllowOverride all              # <-- relax Apache security settings
+ Options -MultiViews            # <-- MultiViews must be turned off
+ </Directory>
+ </VirtualHost>
+ 
+ */
+
 @end
